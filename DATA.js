2358db76e583b8eff609e5300841fef2f5156d61
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-30 17:11:56 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,55 @@
+export const item = {
+    "id": "f3f102a5-d1a5-4d1f-80cc-43cf3d82b7d5",
+    "home_feed": [
+        {
+            "id": "fbe8a069-afb0-4fc3-83bf-5b8de4f42dcb",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png",
+            "name": "particular",
+            "position": "although",
+            "about_post": "Major pull plan. Would new turn road discuss majority least. Level usually similar record.",
+            "post_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png",
+            "like_counts": "physical",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+        },
+        {
+            "id": "c3f8d2c2-5f84-49e0-81ee-055aa96987a2",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png",
+            "name": "it",
+            "position": "yourself",
+            "about_post": "Social memory phone issue. Individual low deal. Life level gas these brother know detail everyone.",
+            "post_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png",
+            "like_counts": "deal",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+        },
+        {
+            "id": "e12adcce-baaf-47b0-9fc8-84d4bd26594d",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png",
+            "name": "stay",
+            "position": "pay",
+            "about_post": "Claim deal television floor central strategy necessary. Act near memory fast nearly young. Brother write production likely blood partner.",
+            "post_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png",
+            "like_counts": "real",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+        },
+        {
+            "id": "8099090f-f6cb-4d8a-8c3f-ccc001ac1ea0",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/27.png",
+            "name": "yet",
+            "position": "call",
+            "about_post": "Nothing speech loss agent. Ability traditional past conference. Yard gas some important.",
+            "post_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png",
+            "like_counts": "resource",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png"
+        },
+        {
+            "id": "d3197e10-62c3-476f-9383-2d30ad9fd9d4",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/150.png",
+            "name": "color",
+            "position": "cost",
+            "about_post": "Example subject both. Girl decision still executive light exactly north. Bag even sit example commercial during.",
+            "post_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/358.png",
+            "like_counts": "parent",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+        }
+    ]
+}
